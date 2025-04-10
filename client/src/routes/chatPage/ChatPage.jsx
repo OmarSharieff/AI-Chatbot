a5dad270a611ccbac33e9bac6f2a1e156a321d41
Omar <omarsharief642002@gmail.com>
2025-04-10 15:06:58 +0530

    Add chat lists

--- client/src/routes/chatPage/ChatPage.jsx
+++ client/src/routes/chatPage/ChatPage.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import { useEffect, useRef } from "react";
+import "./chatPage.css";
+import NewPrompt from "../../components/newPrompt/NewPrompt";
 
 function ChatPage() {
+  
+  const endRef= useRef(null)
+
+  useEffect(()=> {
+    endRef.current.scrollIntoView({behavior:"smooth"});
+  }, [])
+
   return (
-    <div>
-      ChatPage
+    <div className="chatPage">
+      <div className="wrapper">
+        <div className="chat">
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <NewPrompt/>
+          <div ref={endRef}/>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ChatPage
+export default ChatPage;
--- client/src/routes/homepage/Homepage.jsx
+++ client/src/routes/homepage/Homepage.jsx
@@ -1,11 +1,10 @@
-import { Link } from "react-router-dom";
+import { Link, } from "react-router-dom";
 import "./homepage.css";
 import { TypeAnimation } from "react-type-animation";
 import { useState } from "react";
 
 function Homepage() {
-
-  const [typingStatus, setTypingStatus] = useState();
+  const [typingStatus, setTypingStatus] = useState("human1");
 
   return (
     <div className="homepage">
@@ -27,18 +26,39 @@ function Homepage() {
           </div>
           <img src="/bot.png" alt="" className="bot" />
           <div className="chat">
-            <img src="/bot.png" alt="" />
+            <img
+              src={
+                typingStatus === "human1"
+                  ? "/human1.jpeg"
+                  : typingStatus === "human2"
+                  ? "/human2.jpeg"
+                  : "bot.png"
+              }
+              alt=""
+            />
             <TypeAnimation
               sequence={[
                 // Same substring at the start will only be typed out once, initially
-                "Human: We produce food for Mice",
-                1000, // wait 1s before replacing "Mice" with "Hamsters"
-                "Bot: We produce food for Hamsters",
-                1000,
-                "Human2: We produce food for Guinea Pigs",
-                1000,
-                "Bot: We produce food for Chinchillas",
-                1000,
+                "Hey LAMA AI, what exactly do you do?",
+                2000,
+                () => {
+                  setTypingStatus("bot");
+                },
+                "I'm your AI assistant — smart, fast, and ready to help 24/7.",
+                2000,
+                () => {
+                  setTypingStatus("human2");
+                },
+                "Are you really that good?",
+                2000,
+                () => {
+                  setTypingStatus("bot");
+                },
+                "Try me. You’ll wonder how you ever worked without me.",
+                2000,
+                () => {
+                  setTypingStatus("human1");
+                },
               ]}
               wrapper="span"
               repeat={Infinity}
@@ -46,6 +66,13 @@ function Homepage() {
               omitDeletionAnimation={true}
             />
           </div>
+        </div>
+      </div>
+      <div className="terms">
+        <img src="/logo.png" alt="" />
+        <div className="links">
+          <Link to="/">Terms of Service</Link>
+          <Link to="/">Privacy Policy</Link>
         </div>
       </div>
     </div>
